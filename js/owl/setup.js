33683e8c3469d2752a4200a0104a6a6d2e7b2128
Luccas Fernando Almeida da Silva <101434808+Guterw@users.noreply.github.com>
2022-08-14 22:56:49 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+    alert("Olá! Obrigado por visitar meu site, lukinhas agradece 😊 Só pra avisar que to lançando atualizações toda semana, então ele ainda está com bugs e algumas coisas feias kk, lhe desejo uma ótima sessão! 😉🍿 ")
+
